1dc5d50838db777d83f1b5adba5eee4714b84994
Brian Ben Maranville <brian.maranville@nist.gov>
2017-10-26 15:54:04 -0400

    updates to sound play

--- demos/tickplay.js
+++ demos/tickplay.js
@@ -1,94 +1,6 @@
-function BufferLoader(context, urlList, callback) {
-  this.context = context;
-  this.urlList = urlList;
-  this.onload = callback;
-  this.bufferList = new Array();
-  this.loadCount = 0;
-}
+// this is the only part that gets used.
 
-BufferLoader.prototype.loadBuffer = function(url, index) {
-  // Load buffer asynchronously
-  var request = new XMLHttpRequest();
-  request.open("GET", url, true);
-  request.responseType = "arraybuffer";
-
-  var loader = this;
-
-  request.onload = function() {
-    // Asynchronously decode the audio file data in request.response
-    loader.context.decodeAudioData(
-      request.response,
-      function(buffer) {
-        if (!buffer) {
-          alert('error decoding file data: ' + url);
-          return;
-        }
-        loader.bufferList[index] = buffer;
-        if (++loader.loadCount == loader.urlList.length)
-          loader.onload(loader.bufferList);
-      },
-      function(error) {
-        console.error('decodeAudioData error', error);
-      }
-    );
-  }
-
-  request.onerror = function() {
-    alert('BufferLoader: XHR error');
-  }
-
-  request.send();
-}
-
-BufferLoader.prototype.load = function() {
-  for (var i = 0; i < this.urlList.length; ++i)
-  this.loadBuffer(this.urlList[i], i);
-}
-
-var context;
-var bufferLoader;
-
-function sinit() {
-  // Fix up prefixing
-  window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  context = new AudioContext();
-
-  bufferLoader = new BufferLoader(
-    context,
-    [
-      'Tick.mp3',
-    ],
-    finishedLoading
-    );
-
-  bufferLoader.load();
-}
-
-function finishedLoading(bufferList) {
-  var source1 = context.createBufferSource();
-  //var source2 = context.createBufferSource();
-  source1.buffer = bufferList[0];
-  //source2.buffer = bufferList[1];
-
-  source1.connect(context.destination);
-  //source2.connect(context.destination);
-  source1.start(0);
-  //source2.start(0);
-}
-
-function playTick(after) {
-  var after = after || 0;
-  var source1 = context.createBufferSource();
-  //var source2 = context.createBufferSource();
-  source1.buffer = bufferLoader.bufferList[0];
-  //source2.buffer = bufferList[1];
-
-  source1.connect(context.destination);
-  //source2.connect(context.destination);
-  source1.start(after);
-}
-
-var bufsize = 8192;
+var bufsize = 2048;
 var ping = [];
 var ping_length = 3;
 var global_elapsed;
@@ -113,9 +25,7 @@ var ticker = new Pizzicato.Sound({
             var bufend = el + buftime;
             var next_ping = ping.shift();
             while (next_ping && next_ping < bufend) {
-              var st = Math.floor((-next_ping + el)*44.100);
-              //console.log(st);
-              //console.log(next_ping - el);
+              var st = Math.floor((el - next_ping)*44.100);
               for (var i=0; i<ping_length && st >=0 && (st+i)<bufsize; i++) {
                 output[st+i] = 1;
               }
@@ -123,10 +33,6 @@ var ticker = new Pizzicato.Sound({
             }
             // put it back if there's one left over.
             if (next_ping) { ping.splice(0, 0, next_ping) }
-          
-              //output[i] = (i < l/4) ? Math.random() : 0;
-              //output[i] = (i < 6 || (i > 500 && i < 508)) ? 1 : -1;
-              //output[i] = Math.sin(44/2048*Math.PI*i);
         }
     }
 });
