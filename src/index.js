b82c7b56f08a3e2319235b1a4b32dfcb4f6a0936
Brian Ben Maranville <brian.maranville@nist.gov>
2020-04-23 11:32:39 -0400

    adding some js repo files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export {default as heatChart} from './heat-chart.js';
+export {default as xyChart} from './xy-chart.js';
+export {extend} from './jquery-extend.js';
+export {default as rectangleSelect} from './rectangle-select.js';
+export {colormap_names, get_colormap, colormap_data} from './colormap.js';
+export {default as dataflowEditor} from './dataflow-editor.js';
+export {default as xSliceInteractor} from './x-slice-interactor.js';
+export {default as rectangleInteractor} from './rectangle-interactor.js';
+export {default as monotonicFunctionInteractor} from './monotonic-function-interactor.js';
+export {default as ySliceInteractor} from './y-slice-interactor.js';
+export {default as ellipseInteractor} from './ellipse-interactor.js';
+export {default as heatChartMulti} from './heat-chart-multi.js';
+export {linePolygonIntersects, lineLineIntersect, reflect} from './linePolygonIntersect.js';
+export {default as polygonInteractor} from './polygon-interactor.js';
+export {default as profileInteractor} from './profile-interactor.js';
